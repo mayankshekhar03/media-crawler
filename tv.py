5d262980ff79d5ec7cd38b14fedd79f828f95c3c
Mayank Shekhar <mayankshekhar03@live.in>
2017-01-22 20:24:37 +0530

    Update tv.py

--- tv.py
+++ tv.py
@@ -8,23 +8,23 @@ from bs4 import BeautifulSoup as bs
 
 def tv_series_url_list(max_pages):
     page = 1
-    f = open("new_girl.html","w+")
+    f = open("tv_series_name.html","w+")
     f.write("<html>\n<body>\n")
     while page <= max_pages:
-        url = "http://s1.bia2m.biz/Series/New%20Girl/" + "s" + str(page) + "/"
+        url = "http://tv/series/parent/directory" + "s" + str(page) + "/"
         scode = requests.get(url)
         plaintext = scode.text
         soup = bs(plaintext, "lxml")
         for link in soup.find_all('a'):
-            if 'New Girl' in link.string:
-                href = "http://s1.bia2m.biz/Series/New%20Girl/" + "s" + str(page) + "/" + link.get('href')
+            if 'tv series name' in link.string:
+                href = "http://tv/series/parent/directory" + "s" + str(page) + "/" + link.get('href')
                 title = link.string
-                with open('new_girl.html', 'a') as myfile:
+                with open('tv_series_name.html', 'a') as myfile:
                     myfile.write('<a href="'+href+'">'+title+'</a><br>\n')
                 #print(title)
                 #print(href)
         page += 1
-    with open('new_girl.html', 'a') as myfile:
+    with open('tv_series_name.html', 'a') as myfile:
         myfile.write('</body>\n</html>')
 
-tv_series_url_list(7)
+tv_series_url_list(number of seasons)
